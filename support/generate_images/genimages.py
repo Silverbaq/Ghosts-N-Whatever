2b0a6e699c5a34a24124c2a7081fc0bf7d1fcecd
moz <moz@local>
2017-09-30 01:00:59 +0200

    now ouputting all png files

--- support/generate_images/genimages.py
+++ support/generate_images/genimages.py
@@ -13,22 +13,8 @@ def genLocationQr( location, prepend="", path="tmp" ):
     url.png(filename, scale=8)
     return filename
 
-if __name__ == "__main__":
-    villagename = "Cisnadie"
-    imagefile = genLocationQr( villagename , prepend="", path="tmp")
-
-    # from PIL import Image
-    # from PIL import ImageFont
-    # from PIL import ImageDraw
-    #
-    # img = Image.open(imagefile)
-    # draw = ImageDraw.Draw(img)
-    # # font = ImageFont.truetype(<font-file>, <font-size>)
-    # font = ImageFont.truetype("OpenSans-Regular.ttf", 16)
-    # # draw.text((x, y),"Sample Text",(r,g,b))
-    # #draw.text((0, 0),"Sample Text",(255,255,255),font=font)
-    # draw.text((0, 0),"Sample Text",(255,255,255))
-    # img.save('sample-out.jpg')
+def genLocation( location, locationtype, prepend="", path="" ):
+    imagefile = genLocationQr( location, prepend, path=path+"/tmp")
 
     # create canvas
     im_size = (500, 500)
@@ -42,9 +28,22 @@ if __name__ == "__main__":
     offset = ((im_size[0] - im_qrcode_size[0]) / 2, 15)
     im.paste( im_qrcode, offset)
 
+    # add text
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype("OpenSans-Regular.ttf", 60)
-    draw.text((80, 250),villagename,(0,0,0),font=font)
+    draw.text((80, 250),location,(0,0,0),font=font)
 
     # write to file
-    im.save( "location-%s"%villagename, "PNG")
+    filename = ("%s/%s-%s.png"%(path, locationtype, location)).replace(' ', '-')
+    im.save( filename, "PNG")
+    return filename
+
+if __name__ == "__main__":
+    with open( '../largest_towns_romania.txt', 'r') as list:
+        for villagename in list:
+            print "village names: ", villagename.strip()
+            readable_name = villagename.split('\t')[1].strip()
+            print " - using", readable_name
+            fn = genLocation( readable_name, 'village', path='.' )
+
+            print "ristretto %s"%fn
