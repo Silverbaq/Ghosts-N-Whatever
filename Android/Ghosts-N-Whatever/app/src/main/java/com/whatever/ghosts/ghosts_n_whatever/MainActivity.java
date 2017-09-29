ec0c1fd04cf7827f5ad12784247e97a8ade745c1
Steffen Mogensen <feber.sm@gmail.com>
2017-09-29 22:19:26 +0200

    added model

--- Android/Ghosts-N-Whatever/app/src/main/java/com/whatever/ghosts/ghosts_n_whatever/MainActivity.java
+++ Android/Ghosts-N-Whatever/app/src/main/java/com/whatever/ghosts/ghosts_n_whatever/MainActivity.java
@@ -2,12 +2,67 @@ package com.whatever.ghosts.ghosts_n_whatever;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String TAG = "MainActivity";
+
+    TextView tvDisplay;
+    EditText etValue;
+    Button btnClick;
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = (TextView) findViewById(R.id.tvDisplay);
+        btnClick = (Button) findViewById(R.id.btnClick);
+        etValue = (EditText) findViewById(R.id.etValue);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = etValue.getText().toString();
+                // Write a message to the database
+                myRef.setValue(text);
+            }
+        });
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                tvDisplay.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 }
---
+++ Android/Ghosts-N-Whatever/app/src/main/java/com/whatever/ghosts/model/TheRoad.java
@@ -0,0 +1,8 @@
+package com.whatever.ghosts.model;
+
+/**
+ * Created by silverbaq on 29/09/2017.
+ */
+
+public class TheRoad extends Location {
+}
