70e85bc21d3242aaa9d2607e00ba1295fd5f7e8a
Steffen Mogensen <feber.sm@gmail.com>
2017-09-30 01:16:52 +0200

    added layout

---
+++ Android/Ghosts-N-Whatever/app/src/main/java/com/whatever/ghosts/com/whatever/ghosts/fragments/QRScanner.java
@@ -0,0 +1,64 @@
+package com.whatever.ghosts.com.whatever.ghosts.fragments;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class QRScanner extends Fragment implements ZBarScannerView.ResultHandler{
+    final static String TAG = "QRScanner";
+    private ZBarScannerView mScannerView;
+
+    public QRScanner() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mScannerView = new ZBarScannerView(getActivity());
+        return mScannerView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Toast.makeText(getActivity(), "Contents = " + rawResult.getContents() +
+                ", Format = " + rawResult.getBarcodeFormat().getName(), Toast.LENGTH_SHORT).show();
+        // Note:
+        // * Wait 2 seconds to resume the preview.
+        // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
+        // * I don't know why this is the case but I don't have the time to figure out.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.resumeCameraPreview(QRScanner.this);
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+}
---
+++ Android/Ghosts-N-Whatever/app/src/main/java/com/whatever/ghosts/ghosts_n_whatever/ActiveGame.java
@@ -0,0 +1,13 @@
+package com.whatever.ghosts.ghosts_n_whatever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActiveGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_game);
+    }
+}
