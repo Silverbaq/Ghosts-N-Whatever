1b8af7d4cedaadb85874bd43952b26eac95f0f2c
moz <moz@local>
2017-09-30 15:22:10 +0200

    added crypt template and generator - wip

--- support/location_images/genimages.py
+++ support/location_images/genimages.py
@@ -38,19 +38,49 @@ def genLocation( location, locationtype, prepend="", path="" ):
     im.save( filename, "PNG")
     return filename
 
+def genCryptPic( location, locationtype, prepend="", path="" ):
+    backgroundfile = "krypt.png"
+    imagefile = genLocationQr( location, prepend, path=path+"/tmp")
+
+    # create canvas
+    # im_size = (500, 500)
+    # im = Image.new('RGBA', im_size, (255,255,255, 255))
+    im = Image.open( backgroundfile, 'r' )
+    im_size = im.size
+
+    # load qr code½
+    im_qrcode = Image.open( imagefile, 'r')
+    im_qrcode_size = im_qrcode.size
+
+    # add qr code to canvas
+    offset = ((im_size[0] - im_qrcode_size[0]) / 2, 15)
+    im.paste( im_qrcode, offset)
+
+    # add text
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype("OpenSans-Regular.ttf", 40)
+    draw.text((25, 400),prepend+location,(0,0,0),font=font)
+
+    # write to file
+    filename = ("%s/%s-%s.png"%(path, locationtype, location)).replace(' ', '-')
+    im.save( filename, "PNG")
+    return filename
+
 if __name__ == "__main__":
-    with open( '../largest_towns_romania.txt', 'r') as list:
-        for villagename in list:
-            print "village names: ", villagename.strip()
-            readable_name = villagename.split('\t')[1].strip()
-            print " - using", readable_name
-            fn = genLocation( readable_name, 'village', path='.' )
+    # with open( '../largest_towns_romania.txt', 'r') as list:
+    #     for villagename in list:
+    #         print "village names: ", villagename.strip()
+    #         readable_name = villagename.split('\t')[1].strip()
+    #         print " - using", readable_name
+    #         fn = genLocation( readable_name, 'village', path='.' )
+    #
+    #         print "ristretto %s"%fn
+    #
+    fn = genCryptPic( 'Ciobanu', 'crypt', path='.' )
 
-            print "ristretto %s"%fn
-
-    with open( '../romanians_names_top_100.txt', 'r') as list:
-        for surname in list:
-            print "surname: ", surname.strip()
-            fn = genLocation( surname.strip(), 'crypt', prepend="Family crypt of ", path='.' )
-
-            print "ristretto %s"%fn
+    # with open( '../romanians_names_top_100.txt', 'r') as list:
+    #     for surname in list:
+    #         print "surname: ", surname.strip()
+    #         fn = genCryptPic( surname.strip(), 'crypt', prepend="Family crypt of ", path='.' )
+    #
+    #         #print "ristretto %s"%fn
